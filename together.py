628d44b1360c301d4bf57ea94ddddacf4db56ae4
Nosco <EVOFORGE\tmnosco@a-2uol28r7e893e.evoforge.org>
2019-10-17 11:10:58 -0400

    pep

--- together.py
+++ together.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+from tqdm import trange
+from scipy.stats import binom_test
 from Crypto.Util.number import \
     long_to_bytes as l2b, bytes_to_long as b2l
 from functools import reduce
@@ -8,7 +10,8 @@ from common import hook, parse_args, fatal
 import argparse
 import logging
 import random
-import sys, os
+import sys
+import os
 from binascii import hexlify as h
 logger = logging.getLogger(__name__)
 
@@ -22,13 +25,13 @@ def chunks(iterable, n, fillvalue=None, m=None):
     "Collect data into fixed-length chunks or blocks"
     # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx
     args = [iter(iterable)] * n
-    r= it.zip_longest(fillvalue=fillvalue, *args)
-    return map(m,r) if m else r
+    r = it.zip_longest(fillvalue=fillvalue, *args)
+    return map(m, r) if m else r
 
 
 def gen_box_key(num_boxes=4):
     boxes = [
-        linear.main(out=open('/dev/null','w'))
+        linear.main(out=open('/dev/null', 'w'))
         #linear.main(out=sys.stdout)
         for _ in range(num_boxes)
     ]
@@ -48,9 +51,9 @@ def expand_key(key: bytes, num_keys: int = 16):
     keys = b''
     for i in range(num_keys):
         keys += encrypt_block(
-            key, 
-            l2b(i).rjust(len(key),b'\x00')*num_keys, 
-            sbox_key, 
+            key,
+            l2b(i).rjust(len(key), b'\x00')*num_keys,
+            sbox_key,
             len(key)
         )
     return keys, sbox_key
@@ -91,8 +94,8 @@ def apply_sbox(b: int, key: list, block_size: int = 16):
 
 
 def encrypt_round(
-    m: bytes, 
-    key: bytes, sbox_key: bytes, 
+    m: bytes,
+    key: bytes, sbox_key: bytes,
     block_size: int
 ):
     logger.debug("%032x, %032x, %08x", b2l(m), b2l(key), b2l(sbox_key))
@@ -103,8 +106,8 @@ def encrypt_round(
     #apply sbox
     c = apply_sbox(c, sbox_key)
     #bit mixing
-    m = int('f0'*block_size,16)
-    uh, lh = c&m, (c<<4)&m
+    m = int('f0'*block_size, 16)
+    uh, lh = c & m, (c << 4) & m
     c = uh | rol(lh, 8*7-4, block_size*8)
     #test high bit of key
     t = (key & (1 << (block_size-1))) >> (block_size-1)
@@ -115,63 +118,63 @@ def encrypt_round(
 
 
 def encrypt_block(
-    m: bytes, 
-    expanded_key: bytes, sbox_key: bytes, 
+    m: bytes,
+    expanded_key: bytes, sbox_key: bytes,
     block_size: int
 ):
-    return reduce(lambda ct,round_num: 
-        encrypt_round(
-            ct,
-            expanded_key[
-                round_num*block_size:round_num*block_size+block_size
-            ],
-            sbox_key[round_num*4:round_num*4+4],
-            block_size
-        ),
-        range(block_size), m
-        #range(8),m
-    )
+    return reduce(lambda ct, round_num:
+                  encrypt_round(
+                      ct,
+                      expanded_key[
+                          round_num*block_size:(round_num+1)*block_size
+                      ],
+                      sbox_key[round_num*4:(round_num+1)*4],
+                      block_size
+                  ),
+                  range(block_size), m
+                  #range(8),m
+                  )
 
-from tqdm import trange
-from scipy.stats import binom_test
 
 def analysis(key):
     keys, sbox = expand_key(key)
     x, y = b2l(os.urandom(16)), b2l(os.urandom(16))
     diff = b2l(os.urandom(16))
     diff_sqrd = []
-    for i in (x,y):
-        a = b2l(encrypt_block(l2b(i),keys,sbox,16))
-        b = b2l(encrypt_block(l2b(i^diff),keys,sbox,16))
+    for i in (x, y):
+        a = b2l(encrypt_block(l2b(i), keys, sbox, 16))
+        b = b2l(encrypt_block(l2b(i ^ diff), keys, sbox, 16))
         print("{:032x}->{:032x}".format(i, a))
-        print("{:032x}->{:032x}".format(i^diff, b))
-        print("{:032x}->{:032x}".format(diff, a^b))
+        print("{:032x}->{:032x}".format(i ^ diff, b))
+        print("{:032x}->{:032x}".format(diff, a ^ b))
         print("-"*66)
-        diff_sqrd.append(a^b)
-    print("{:032x}".format(diff_sqrd[0]^diff_sqrd[1]).rjust(66,' '))
+        diff_sqrd.append(a ^ b)
+    print("{:032x}".format(diff_sqrd[0] ^ diff_sqrd[1]).rjust(66, ' '))
 
     bits = [0 for _ in range(16*8)]
     sample = 5000
     for _ in trange(sample):
         diff_sqrd = []
-        x,y = b2l(os.urandom(16)),b2l(os.urandom(16))
-        for i in (x,y):
-            a = b2l(encrypt_block(l2b(i),keys,sbox,16))
-            b = b2l(encrypt_block(l2b(i^diff),keys,sbox,16))
-            diff_sqrd.append(a^b)
-        r = diff_sqrd[0]^diff_sqrd[1]
+        x, y = b2l(os.urandom(16)), b2l(os.urandom(16))
+        for i in (x, y):
+            a = b2l(encrypt_block(l2b(i), keys, sbox, 16))
+            b = b2l(encrypt_block(l2b(i ^ diff), keys, sbox, 16))
+            diff_sqrd.append(a ^ b)
+        r = diff_sqrd[0] ^ diff_sqrd[1]
 
         #r = encrypt_block(os.urandom(16), keys, sbox, 16)
         r = '{:0128b}'.format(r)
-        bits = [x+int(y,2) for x,y in zip(bits,r)]
+        bits = [x+int(y, 2) for x, y in zip(bits, r)]
 
     test = ('{:03d}'.format(
-        int(binom_test(x,sample,0.5)*100)
+        int(binom_test(x, sample, 0.5)*100)
     ) for x in bits)
-    print('\n'.join(chunks(test,16,m=' '.join)))
+    print('\n'.join(chunks(test, 16, m=' '.join)))
+
 
 def main(out, **kwargs):
     logger.debug("main(%r)", locals())
+
 
 if __name__ == "__main__":
     #make the argument parser
